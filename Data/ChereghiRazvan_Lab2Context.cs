d44966768afccb865bd0aaf3c7fa528d478da8ef
RazvanChereghi <razvanchereghi@yahoo.com>
2023-11-25 16:21:03 +0200

    Lab3

--- Data/ChereghiRazvan_Lab2Context.cs
+++ Data/ChereghiRazvan_Lab2Context.cs
@@ -19,5 +19,7 @@ namespace ChereghiRazvan_Lab2.Data
         public DbSet<ChereghiRazvan_Lab2.Models.Publisher>? Publisher { get; set; }
 
         public DbSet<ChereghiRazvan_Lab2.Models.Author>? Author { get; set; }
+
+        public DbSet<ChereghiRazvan_Lab2.Models.Category>? Category { get; set; }
     }
 }
